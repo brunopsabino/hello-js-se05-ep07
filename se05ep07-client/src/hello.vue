82d674caf885a8deea3d0e022b907cb7855482a6
Bruno Sabino <bruno.p.sabino@gmail.com>
2018-02-03 12:07:39 -0200

    alterar layout para teste

--- se05ep07-client/src/hello.vue
+++ se05ep07-client/src/hello.vue
@@ -4,20 +4,21 @@
     <md-layout md-gutter>
       <md-card md-with-hover>
         <md-card-header>
-          <div class="md-title">Hello world vue-material</div>
-          <div class="md-subhead">Almighty counter</div>
+          <div class="md-title">Contador</div>
+          <div class="md-subhead">Construído com Vue Material</div>
         </md-card-header>
         <md-card-content>
           {{counter}}
         </md-card-content>
         <md-card-actions>
-          <md-button @click="counter++">Action</md-button>
+          <md-button @click="counter++">Adicionar</md-button>
         </md-card-actions>
       </md-card>
     </md-layout>
     <md-layout md-gutter></md-layout>
   </md-layout>
 </template>
+
 <script>
 module.exports = {
   name: "HelloVue",
